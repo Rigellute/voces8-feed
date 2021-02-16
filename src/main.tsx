dc5f5abd5d193fdb05e55da548e801840841b2f7
Alexander Keliris <rigellute@gmail.com>
2021-02-16 18:29:40 +0000

    Use entrypoint id "voces8-feed"

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
-import { render, h } from 'preact'
-import { App } from './app'
-import './index.css'
+import { render, h } from "preact";
+import { App } from "./app";
+import "./index.css";
 
-render(<App />, document.getElementById('app')!)
+render(<App />, document.getElementById("voces8-feed")!);
