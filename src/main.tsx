8000dc195c9258a924f646705ec179ed832219d9
Alexander Keliris <rigellute@gmail.com>
2021-05-05 14:08:33 +0100

    Wrap `render` in `Squarespace.onInitialize`

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,11 @@ import { render, h } from "preact";
 import { App } from "./app";
 import "./index.css";
 
-render(<App />, document.getElementById("voces8-feed")!);
+/**
+ * Squarespace loads pages via ajax, so this script might not be executed when we reach the right page.
+ * Squarespace provides an `onInitialize` function that gets run when the ajax page has loaded
+ */
+// @ts-ignore
+window.Squarespace.onInitialize(Y, () => {
+    render(<App/>, document.getElementById("voces8-feed")!);
+});
